17264b1175e8388af87ab02dc12de48f30442ba1
Roni Kreinin <rkreinin@clearpathrobotics.com>
2023-01-24 16:36:27 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
-from setuptools import setup
-import pyuavcan_v0.version as version
+from setuptools import setup, find_packages
 
 setup(
     name="pyuavcan_v0",
-    packages=["pyuavcan_v0", "pyuavcan_v0.app", "pyuavcan_v0.driver", "pyuavcan_v0.dsdl", "pyuavcan_v0.dsdl_files"],
-    version=version.__version__,
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    include_package_data=True,
+    version='1.0.35',
     install_requires=[
         'importlib-metadata; python_version == "3.2"',
     ],
