10da65355001f1c4e1e243811bfba9f61a0a782d
Roni Kreinin <rkreinin@clearpathrobotics.com>
2023-01-24 15:59:41 -0500

    Added dsdl_files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import pyuavcan_v0.version as version
 
 setup(
     name="pyuavcan_v0",
-    packages=["pyuavcan_v0", "pyuavcan_v0.app", "pyuavcan_v0.driver", "pyuavcan_v0.dsdl"],
+    packages=["pyuavcan_v0", "pyuavcan_v0.app", "pyuavcan_v0.driver", "pyuavcan_v0.dsdl", "pyuavcan_v0.dsdl_files"],
     version=version.__version__,
     install_requires=[
         'importlib-metadata; python_version == "3.2"',
